7e29e525307e992dfa0a81d48b33d10ed77955da
zhangyu03121011@163.com <zhangyu03121011@163.com>
2017-07-06 09:44:48 +0800

    Signed-off-by: zhangyu03121011@163.com <zhangyu03121011@163.com>

--- src/main/java/com/test/MyApplication.java
+++ src/main/java/com/test/MyApplication.java
@@ -4,10 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-@Configuration
 public class MyApplication {
 	private static Logger logger = LoggerFactory.getLogger(MyApplication.class);
 	public static void main(String[] args) {
